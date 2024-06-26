3b0026b4c55e7028b5034ef39a22fe1e47815a8f
Khizar Abbasi <khizerabbasi144@gmail.com>
2024-06-26 09:10:14 +0500

    new things Added

--- app/src/main/java/com/tenx/expancemanager/ui/activity/DashboardActivity.kt
+++ app/src/main/java/com/tenx/expancemanager/ui/activity/DashboardActivity.kt
@@ -3,8 +3,6 @@ package com.tenx.expancemanager.ui.activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.tenx.expancemanager.R
 import com.tenx.expancemanager.databinding.ActivityDashboardBinding
